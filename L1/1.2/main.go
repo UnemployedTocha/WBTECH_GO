66df1b62851db9b50677cabeda4888ba4274d759
UnemployedTocha <UnemployedTocha@mail.ru>
2025-09-17 17:11:50 +0700

    Add: L1/1.2 task

---
+++ L1/1.2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Square(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(num*num)
+}
+
+func main() {
+	arr := [5]int{2, 4, 6, 8, 10}
+	var wg sync.WaitGroup
+
+
+	for _, num := range arr {
+		wg.Add(1)
+		go Square(num, &wg)	
+	}
+	
+	wg.Wait()
+
+	fmt.Println("hello!!!")
+}
